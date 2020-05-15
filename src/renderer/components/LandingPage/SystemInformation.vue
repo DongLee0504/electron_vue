887eba7c9e63afd9472da3ff1f060c8393191f5b
donglee <wx_169b9269b31944e4a0196f99ad0f6c53@git.code.tencent.com>
2020-05-15 09:27:59 +0800

    1.0.5

--- src/renderer/components/LandingPage/SystemInformation.vue
+++ src/renderer/components/LandingPage/SystemInformation.vue
@@ -49,7 +49,7 @@ const { ipcRenderer } = require('electron')
         console.log(data);
       })
       ipcRenderer.on('downloadProgress', (e, data) => {
-        console.log('我变化了')
+        console.log('1.0.5')
         console.log(data)
       })
     },
